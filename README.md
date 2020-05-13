8ddae7a99b51071fc3c253a07db9e531e16a3d90
nikhil <iit2018152@iiita.ac.in>
2020-05-13 18:40:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#Tutorial from where i learned to build this bot
+
+Link to article on SitePoint: [https://www.sitepoint.com/discord-bot-node-js/](https://www.sitepoint.com/discord-bot-node-js/)
+
+## Requirements
+
+- [Node.js](http://nodejs.org/)
+- [Discord](https://discordapp.com/) account
+
+## Installation Steps (if applicable)
+
+1. Clone repo
+2. Run `npm install`
+3. Add Discord credentials in a `.env` file
+3. Run `node index.js`
+4. Interact with your Discord bot via your web browser
+
+
---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+    msg.channel.send('pong');
+
+  } else if (msg.content.startsWith('!kick')) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  } else if (msg.content === 'flag')
+   {
+   //	msg.reply('jctf{i_was_just_promoted_as_bot}');
+   	msg.channel.send('jctf{i_was_just_promoted_as_bot}');
+   }
+});
